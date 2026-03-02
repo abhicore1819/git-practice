c5221fdc1cd62b3afdd9c7a244015df8a3ba1361
Abhinav Chaudhary <abhicore1819@gmail.com>
2026-03-02 17:50:10 +0530

    added a new python file named index.py

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hey i'm a new js file, i do nothing !")
+
+const txt = document.getElementById("txt");
+const btn = document.getElementById("btn");
+const feedback = document.getElementById("feedback");
+const copyToClipboar = () => {
+  navigator.clipboard
+    .writeText(txt.innerText)
+    .then(() => {
+      console.log("this is the text !");
+      feedback.innerText = "Text copied to clipboard 🎉";
+    //   setTimeout(() => {
+    //     feedback.innerText = "";
+    //   }, 2000);
+    })
+    .catch((err) => {
+      console.log("error occured:-", err);
+    });
+  console.log("working");
+};
+btn.addEventListener("click", copyToClipboar);
